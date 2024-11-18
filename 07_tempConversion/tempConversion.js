e02aefbac03bbb17f6a12a7ba6efa01636e8b963
DanielCHodgson <45772010+DanielCHodgson@users.noreply.github.com>
2024-11-18 21:14:32 +0000

    exercises 7 and 8 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+
+  let unroundedCelcius = ((temp - 32) * 5) / 9
+  let roundedTemp = Math.round(unroundedCelcius * 10) / 10;
+  return roundedTemp;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let unroundedFarenheit = (temp * 1.8) + 32;
+  let roundedTemp = Math.round(unroundedFarenheit * 10) / 10;
+  return roundedTemp;
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,55 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const subtract = function (num1, num2) {
+  return num1 - num2;
 
 };
 
-const power = function() {
-	
+const sum = function (nums) {
+
+  var numbers = nums.map(Number);
+  let sum = 0;
+  numbers.forEach(num => sum += num);
+  return sum;
 };
 
-const factorial = function() {
-	
+const multiply = function (nums) {
+
+  let total = nums[0];
+
+  for (i = 1; i < nums.length; i++) {
+    total = nums[i] * total;
+  }
+
+  return total;
+
+};
+
+const power = function (num, power) {
+
+  let total = num;
+
+  for (i = 1; i < power; i++) {
+    total = num * total;
+  }
+
+  return total;
+};
+
+const factorial = function (num) {
+ 
+  let factoral = 1;
+  
+  if (num === 0) return factoral;
+
+  for (i = 1; i <= num; i++) {
+    factoral = factoral * i;
+  }
+
+  return factoral;
+
 };
 
 // Do not edit below this line
